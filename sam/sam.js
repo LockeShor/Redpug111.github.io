ed6ceb431a4b38b0809676b32dc2bde866189544
Redpug111 <75901583+Redpug111@users.noreply.github.com>
2021-10-21 18:04:19 -0400

    bruh why no worky

--- sam/sam.js
+++ sam/sam.js
@@ -2,5 +2,6 @@ myStorage = window.localStorage;
 
 if(!myStorage.getItem('sam')) {
     myStorage.setItem('sam', 'hogan?')
+    console.log("created sam! :)")
 }
 console.log(myStorage.getItem('sam'))
