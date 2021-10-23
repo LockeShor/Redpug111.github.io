08e170ba2d4f472d001f9d4fa3a2763410270d70
Redpug111 <mrdoodly111@gmail.com>
2021-10-23 14:01:51 -0400

    hehe commit man go brr

--- sam/sam.js
+++ sam/sam.js
@@ -1,9 +1,29 @@
-myStorage = window.localStorage;
+var recX=0;
+var recY=0;
+var keydown;
 
-if(!myStorage.getItem('sam')) {
-    myStorage.setItem('sam', 'hogan?')
-    console.log("created sam! :)")
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log(myStorage.getItem('sam'))
-console.log("status: " + window.status)
+function draw() {
+    var canvas = document.getElementById('tutorial');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        setInterval(function(){
+            if(recX < 400) {
+                recX+= 5;
+                recY+= 5;
+            }
+            else {
+                recX = -50;
+                recY = -50;
+            }
+            
+            rand = randomNumber(150,150)
+            ctx.clearRect(0,0,400,400)
+            ctx.fillRect(parseInt(rand), parseInt(rand),50, 50)
+            console.log(parseInt(rand))
+        }, 30)
+    }
+}
